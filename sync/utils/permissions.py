c61a4a092ae9a1e5942ef770b452f4fb531be370
NUCCASJNR <adegbitealareef28@yahoo.com>
2024-04-01 22:30:16 +0100

    Added custom permissions

--- sync/utils/permissions.py
+++ sync/utils/permissions.py
@@ -1,28 +1,55 @@
+#!/usr/bin/env python3
+
+"""
+contains custom permissions for the sync app
+"""
+
 from rest_framework.permissions import BasePermission
+from sync.models.permission import Document, Permission
 
-
-class IsOwner(BasePermission):
+class HasEditAccess(BasePermission):
     """
-    Custom permission to only allow owners to access the view.
+    Custom permission to only allow users to edit document
     """
-
+    
     def has_permission(self, request, view):
-        return request.user.role == 'Owner'
+        """
+        Check if the requesting user has edit access for the document.
+        """
+        document_id = request.data.get('document_id')
+        if not document_id:
+            return False
+        try:
+            document = Document.custom_get(id=document_id)
+        except Document.DoesNotExist:
+            return False
+        user = request.user
+        try:
+            permission = Permission.objects.get(document=document, user=user)
+            return permission.can_edit
+        except Permission.DoesNotExist:
+            return False
 
 
-class IsTenant(BasePermission):
+class HasCommentAccess(BasePermission):
     """
-    Custom permission to only allow tenants to access the view.
+    Custom permission to only allow users to comment on document
     """
-
+    
     def has_permission(self, request, view):
-        return request.user.role == 'Tenant'
-
-
-class IsAgent(BasePermission):
-    """
-    Custom permission to only allow agents to access the view.
-    """
-
-    def has_permission(self, request, view):
-        return request.user.role == 'Agent'
+        """
+        Check if the requesting user has comment access for the document.
+        """
+        document_id = request.data.get('document_id')
+        if not document_id:
+            return False
+        try:
+            document = Document.custom_get(id=document_id)
+        except Document.DoesNotExist:
+            return False
+        user = request.user
+        try:
+            permission = Permission.objects.get(document=document, user=user)
+            return permission.can_comment
+        except Permission.DoesNotExist:
+            return False
